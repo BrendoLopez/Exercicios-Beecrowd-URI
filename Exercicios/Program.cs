f5f1a46da06926c252c63cc06b4b5578a268adb9
Brendo Lopez <brendoslopes@gmail.com>
2024-01-13 08:00:39 -0300

    Mais exercicios.

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,48 +1,66 @@
 ﻿using System;
-using System.Diagnostics.Metrics;
-using System.Net;
+using System.Globalization;
 
 class URI
 {
     static void Main(string[] args)
     {
-        // Sequência de Números e Soma
-        // Exercício Beecrowd/URI 1101 - C#.
+        //Maria acabou de iniciar seu curso de graduação na faculdade de medicina e precisa de sua ajuda para organizar os experimentos 
+        //de um laboratório o qual ela é responsável.Ela quer saber no final do ano, quantas cobaias foram utilizadas no laboratório 
+        //e o percentual de cada tipo de cobaia utilizada.
 
-        //Leia um conjunto não determinado de pares de valores M e N(parar quando algum dos valores for menor ou igual a zero).
-        //Para cada par lido, mostre a sequência do menor até o maior e a soma dos inteiros consecutivos entre eles(incluindo o N e M).
+        //Este laboratório em especial utiliza três tipos de cobaias: sapos, ratos e coelhos.Para obter estas informações,
+        //ela sabe exatamente o número de experimentos que foram realizados, o tipo de cobaia utilizada e a quantidade
+        //de cobaias utilizadas em cada experimento.
 
-        //*Entrada
-        //O arquivo de entrada contém um número não determinado de valores M e N.A última linha de entrada vai conter um número nulo ou negativo.
+        //Entrada ----->
+        //A primeira linha de entrada contém um valor inteiro N que indica os vários casos de teste que vem a seguir.
+        //Cada caso de teste contém um inteiro Quantia(1 ≤ Quantia ≤ 15) que representa a quantidade de cobaias utilizadas
+        //e um caractere Tipo('C', 'R' ou 'S'), indicando o tipo de cobaia(R: Rato S: Sapo C: Coelho).
 
-        //*Saída
-        //Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
+        //Saída <------
+        //Apresente o total de cobaias utilizadas, o total de cada tipo de cobaia utilizada e o percentual de cada uma
+        //em relação ao total de cobaias utilizadas, sendo que o percentual deve ser apresentado com dois dígitos após o ponto.
 
-        string[] vetor = Console.ReadLine().Split(' ');
-        int M = int.Parse(vetor[0]);
-        int N = int.Parse(vetor[1]);
+        int N = int.Parse(Console.ReadLine());
 
-        int soma = 0;
-        while (M > 0 && N > 0)
+        int totalCobaias = 0;
+        int totalCoelhos = 0;
+        int totalRatos = 0;
+        int totalSapos = 0;
+
+        for (int i = 0; i < N; i++)
         {
-            int menor = Math.Min(M, N);
-            int maior = Math.Max(M, M);
+            string[] vetor = Console.ReadLine().Split(' ');
+            int quantidadeAnimal = int.Parse(vetor[0]);
+            char tipoAnimal = char.Parse(vetor[1]);
 
-            vetor = Console.ReadLine().Split(' ');
-            M = int.Parse(vetor[0]);
-            N = int.Parse(vetor[1]);
-
-            for (int i = menor; i <= maior; i++)
+            if (tipoAnimal == 'C')
             {
-                Console.Write(i + " ");
-                soma += i;
+                totalCoelhos += quantidadeAnimal;
             }
-            Console.WriteLine($"Sum={soma}");
+            else if (tipoAnimal == 'R')
+            {
+                totalRatos += quantidadeAnimal;
+            }
+            else if (tipoAnimal == 'S')
+            {
+                totalSapos += quantidadeAnimal;
+            }
+            totalCobaias += quantidadeAnimal;
         }
 
-        if (M <= 0 || N <= 0)
-        {
-            Console.WriteLine("M ou N menor ou igual a 0");
-        }
+        Console.WriteLine("Total: " + totalCobaias + " cobaias");
+        Console.WriteLine("Total de coelhos: " + totalCoelhos);
+        Console.WriteLine("Total de ratos: " + totalRatos);
+        Console.WriteLine("Total de sapos: " + totalSapos);
+
+        double percentualCoelhos = (double)totalCoelhos / totalCobaias * 100;
+        double percentualRatos = (double)totalRatos / totalCobaias * 100;
+        double percentualSapos = (double)totalSapos / totalCobaias * 100;
+
+        Console.WriteLine("Percentual de coelhos: " + percentualCoelhos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+        Console.WriteLine("Percentual de ratos: " + percentualRatos.ToString("F2", CultureInfo.InvariantCulture) + " %");
+        Console.WriteLine("Percentual de sapos: " + percentualSapos.ToString("F2", CultureInfo.InvariantCulture) + " %");
     }
 }
